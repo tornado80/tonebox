f2429fd42e0c4e5499aec74d484377e578c7a344
Mohammad Mousavi <smmm.aut.ac.ir>
2020-07-08 22:41:20 +0430

    wrote the init method for song

--- core/songs_manager.py
+++ core/songs_manager.py
@@ -1,5 +1,20 @@
 class Song:
-    pass
+     def __init__(self, path, id_=None, artist=None, title=None, album=None, track_total=None, duration=None, genre=None, year=None, composer=None, filesize=None, bitrate=None, samplerate=None, comment=None, image=None):
+        self.path = path
+        self.id = id_
+        self.artist = artist
+        self.title = title
+        self.album = album
+        self.track_total = track_total
+        self.duration = duration
+        self.genre = genre
+        self.year = year
+        self.composer = composer
+        self.filesize = filesize
+        self.bitrate = bitrate
+        self.samplerate = samplerate
+        self.comment = comment
+        self.image = image
 
 class SongsManager:
     pass
