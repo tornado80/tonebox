1843359967f2bc166da90b4f9b780f9852a79b8b
Mohammad Mousavi <smmm.aut.ac.ir>
2020-07-15 00:15:26 +0430

    added an id attribute for playlist and add and remove playlist for playlistManger

--- core/playlists_manager.py
+++ core/playlists_manager.py
@@ -5,6 +5,7 @@ import ntpath
 class Playlist:
     def __init__(self, name, last_played=None):
         self.name = name
+        self.id_ = id(self)
         self.songs = []
         self.c_date = datetime.datetime.now()
         self.last_played = last_played
@@ -18,8 +19,26 @@ class Playlist:
 class PlaylistManager:
     def __init__(self, db_path):
         self.db_path = db_path
-        self.db_name = ntpath.basename(db_path)
-
         self.conn = sqlite3.connect(self.db_path) 
         self.c = self.conn.cursor()
+
+    def add_playlist(self, playlist):
+        try:
+            self.c.execute(f"INSERT INTO playlists VALUES ({playlist.id_}, {playlist.name})")    #assumes the table name is playlists if its not it should be changed
+        except Exception as e:
+            return repr(e)
+        else:
+            self.conn.commit()
+
+    def remove_playlist(self, playlist):
+        try:
+            self.c.execute(f"DELETE FROM playlists WHERE id=?", (playlist.id_,))
+        except Exception as e:                                                         #should also remove the playlists keys from the group table 
+            return repr(e)
+        else:
+            self.conn.commit()
+
+                
+
+
         
